a957a528518a640b58e79243d2920b6860aa1161
CYRiXplaysHalo <43896332+CYRiXplaysHalo@users.noreply.github.com>
2019-09-22 16:45:46 -0400

    Add map to output file name and ..
    Add map to output file name and allow series score files to be updated externally without breaking things.

--- Scrape_PGCR_From_Device.py
+++ Scrape_PGCR_From_Device.py
@@ -23,6 +23,7 @@ print(success)
 cv2.imwrite(timestamp + " - Test Screenshot.png", image)
 height, width, channels = image.shape
 banner_height = int((80/480)*height)
+footer_height = int((400/480)*height)
 pgcr_flag = 0
 
 #print("Video Resolution = " + str(height) + "x" + str(width))
@@ -47,11 +48,30 @@ while success:
     #if the text we are looked for is there, save screenshot with video timestamp
     if "POSTGAME CARNAGE REPORT" in text and pgcr_flag == 0:
         print("PGCR Present at frame " + str(count))
-        cv2.imwrite(timestamp + " - PGCR.png", image)
+	
+	#Perform OCR on footer of screen to get file name
+	footer = image[footer_height:height, 0:width-140] #must omit right part incase timer or prompts appear
+        gray = cv2.cvtColor(footer, cv2.COLOR_BGR2GRAY)
+        footer_text = pytesseract.image_to_string(gray)
+        footer_text = footer_text.strip()
+        footer_text = ''.join(x for x in footer_text if x.isalpha())
+	
+	#output screen shot
+        cv2.imwrite(timestamp + " - " + footer_text + " - PGCR.png", image)
         gray2 = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         pgcr_text = pytesseract.image_to_string(cv2.threshold(gray2, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1])
         print(pgcr_text)
         text_arr = pgcr_text.split('\n')
+	
+	#read in score files incase scores were edited
+	file = open("red_team_series_score.txt","r")
+        red_team_series_score = int(file.read())
+
+        file = open("blue_team_series_score.txt","r")
+        blue_team_series_score = int(file.read())
+	
+	#whichever team shows up first is the one that one
+	#no this doesn't account for weird tie situations
         for row in text_arr:
             if "Blue Team" in row:
                     blue_team_series_score += 1
@@ -60,6 +80,7 @@ while success:
                     red_team_series_score += 1
                     break
 
+	#update score files
         file = open("red_team_series_score.txt","w") 
         file.write(str(red_team_series_score)) 
         file.close()
